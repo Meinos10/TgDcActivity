cadbc56bccdfdd9075a3cd9316db7ef1f52d0341
ReWoxi <64690769+Meinos10@users.noreply.github.com>
2022-12-22 23:35:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = #Discord Bot Token
+prefix = "!"
+
+tg = #Telegram Bot Token
+api_id = #my.telegram.org app_id
+api_hash= #my.telegram.org api_hash
+message_id = #edit message id
+chat= #telegram chat id
